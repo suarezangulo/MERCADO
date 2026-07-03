ef054e17387881fdd1f541355f5812db1b8eceee
suarezangulo <106022685+suarezangulo@users.noreply.github.com>
2026-07-03 13:44:27 -0400

    Create upload-images.php

---
+++ data/upload-images.php
@@ -0,0 +1,22 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+
+$uploadDir = __DIR__ . '/../images/products/';
+if (!is_dir($uploadDir)) {
+    mkdir($uploadDir, 0777, true);
+}
+
+$uploaded = [];
+foreach ($_FILES['images']['tmp_name'] as $key => $tmpName) {
+    $originalName = $_FILES['images']['name'][$key];
+    $extension = pathinfo($originalName, PATHINFO_EXTENSION);
+    $newName = pathinfo($originalName, PATHINFO_FILENAME) . '.' . $extension;
+    $destination = $uploadDir . $newName;
+    if (move_uploaded_file($tmpName, $destination)) {
+        $uploaded[] = $newName;
+    }
+}
+
+echo json_encode(['success' => true, 'uploaded' => $uploaded, 'count' => count($uploaded)]);
